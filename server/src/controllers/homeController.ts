1d87c901feb966a9d6f1113b6159f866dda96a79
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2025-07-04 11:27:32 -0500

    updated premium upgrade banner, altered color theme

--- server/src/controllers/homeController.ts
+++ server/src/controllers/homeController.ts
@@ -2,9 +2,8 @@ const expressAsyncHandler = require("express-async-handler");
 const { supabase } = require("../supabase.ts"); // adjust path if needed
 
 exports.fetchPractices = expressAsyncHandler(async (req, res, next) => {
-	console.log("fetch practices runs");
 	const { data, error } = await supabase.from("Practice").select("*");
-
+	console.log(data, 'data from supabase')
 	if (error) {
 		console.error("Supabase error:", error);
 		return res.status(500).json({ error: error.message });
